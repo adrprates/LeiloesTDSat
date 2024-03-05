206286b5b85c9cbb6a2669b0814f2f479c71efe5
Adriano Prates <adrianopratessouza@gmail.com>
2024-03-05 11:36:09 -0300

    Definindo cadastroVIEW como tela principal

--- LeiloesTDSat/src/LeiloesTDSat.java
+++ LeiloesTDSat/src/LeiloesTDSat.java
@@ -4,4 +4,10 @@
  */
 public class LeiloesTDSat {
     
+    public static void main(String[] agrs){
+        //definindo a tela inicial do sistema
+        cadastroVIEW telaInicial = new cadastroVIEW();
+        telaInicial.setVisible(true);
+    }
+    
 }
