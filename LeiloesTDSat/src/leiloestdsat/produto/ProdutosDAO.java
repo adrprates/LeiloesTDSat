ff01117c1971cb9dcb28058958453fb143c23366
Adriano Prates <adrianopratessouza@gmail.com>
2024-03-07 11:13:13 -0300

    Criação do método de cadastrar produto no banco de dados

--- LeiloesTDSat/src/leiloestdsat/produto/ProdutosDAO.java
+++ LeiloesTDSat/src/leiloestdsat/produto/ProdutosDAO.java
@@ -1,44 +1,41 @@
 package leiloestdsat.produto;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import leiloestdsat.conexaojdbc.conectaDAO;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Adm
  */
-
-import java.sql.PreparedStatement;
-import java.sql.Connection;
-import javax.swing.JOptionPane;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
-
 public class ProdutosDAO {
     
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
-    }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
+        try{
+            //criando objeto para fazer conexao com o banco de dados
+            conectaDAO conexao = new conectaDAO();
+            
+            //conexao com o banco de dados
+            conexao.conectar();
+            
+            //instrucao SQL que sera executada
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES "
+                    + "(?, ?, ?)";
+            PreparedStatement consulta = conexao.getConexao().prepareStatement(sql);
+            consulta.setString(1, produto.getNome());
+            consulta.setDouble(2, produto.getValor());
+            consulta.setString(3, produto.getStatus());
+            
+            //executa instrucao SQL
+            consulta.execute();
+            
+            //desconexao com o banco de dados
+            conexao.desconectar();
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao tentar cadastrar"
+                    + " produto no banco de dados" + e.getMessage());
+        }
+    }     
 }
 
