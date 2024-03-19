77eae39b2b500568196124fc9a61721e0b3fd961
Adriano Prates <adrianopratessouza@gmail.com>
2024-03-19 15:38:22 -0300

    Criação da classe ProdutosListagemVendasDAO contendo o método para listar os produtos que foram vendidos

---
+++ LeiloesTDSat/src/leiloestdsat/produto/ProdutosListagemVendasDAO.java
@@ -0,0 +1,55 @@
+package leiloestdsat.produto;
+
+import java.util.ArrayList;
+import java.util.List;
+import leiloestdsat.conexaojdbc.conectaDAO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adriano
+ */
+public class ProdutosListagemVendasDAO {
+    
+    //metodo para listagem de produtos vendidos
+    public static List<ProdutosDTO> listarProdutosVendidos(){
+        //criando lista de produtos
+        List<ProdutosDTO> lista = new ArrayList<ProdutosDTO>();
+        try{
+            //criando objeto para fazer conexao com o banco de dados
+            conectaDAO conexao = new conectaDAO();
+            
+            //conexao com o banco de dados
+            conexao.conectar();
+            
+            //instrucao SQL que sera executada
+            String sql = "SELECT * FROM produtos WHERE status = 'Vendido';";
+            PreparedStatement consulta = conexao.getConexao().prepareStatement(sql);
+            
+            //executa instrucao SQL
+            ResultSet resposta = consulta.executeQuery();
+            
+            while(resposta.next()){
+                ProdutosDTO p = new ProdutosDTO();
+                
+                p.setId(resposta.getInt("id"));
+                p.setNome(resposta.getString("nome"));
+                p.setValor(resposta.getBigDecimal("valor"));
+                p.setStatus(resposta.getString("status"));
+                
+                lista.add(p);
+            }
+            
+            //desconexao com o banco de dados
+            conexao.desconectar();
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao tentar listar"
+                    + " produtos vendidos no banco de dados" + e.getMessage());
+        }
+        
+        return lista;
+    }
+}
